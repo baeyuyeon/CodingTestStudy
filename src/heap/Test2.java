1dc8d1814a87e11159bb30f9038e9c1d40843664
Baeyuyeon <dbdus983@naver.com>
2023-03-16 21:49:11 +0900

    heap 문제 2개 추가

--- src/heap/Test2.java
+++ src/heap/Test2.java
@@ -4,20 +4,36 @@ import java.util.*;
 public class Test2 {
     public static void main(String[] args) {
         int[][] jobs = {{0, 5}, {2, 10}, {10000, 2}};
+
         Test2 test2=new Test2();
         System.out.println(test2.solution(jobs));
     }
     public int solution(int[][] jobs) {
+
         int answer = 0;
         Queue<Job> timeQueue = new PriorityQueue<>(
-                (a, b)->{return a.time-b.time;}
+                (a, b)->{
+                    if(a.time==b.time){
+                        return a.position-b.position;
+                    }else{
+                        return a.time-b.time;
+                    }
+                }
         );
+
         Queue<Job> positionQueue = new PriorityQueue<>(
-                (a, b)->{return a.position-b.position;}
+                (a, b)->{
+                    if(a.position==b.position){
+                        return a.time-b.time;
+                    }else{
+                        return a.position-b.position;
+                    }
+                }
         );
         for(int i=0; i<jobs.length; i++){
             positionQueue.offer(new Job(jobs[i][0], jobs[i][1]));
         }
+        System.out.println(positionQueue);
         Job first = positionQueue.poll();
         int current =first.position+ first.time;
         int sum=first.time;
@@ -34,7 +50,7 @@ public class Test2 {
                 }else{
                     job=positionQueue.poll();
                     if(job.position>current){
-                        sum = sum+job.time+(job.position);
+                        sum = sum+job.time;
                     }else{
                         sum = sum+job.time+(current- job.position);
                     }
@@ -52,6 +68,27 @@ public class Test2 {
         Job(int position, int time){
             this.position = position;
             this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Job{" +
+                    "position=" + position +
+                    ", time=" + time +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Job)) return false;
+            Job job = (Job) o;
+            return position == job.position && time == job.time;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(position, time);
         }
     }
 }
