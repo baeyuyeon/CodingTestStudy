61fa68de9ac86fc3daec37cedce2d817b6537472
baeyuyeon <dbdus983@naver.com>
2023-03-25 16:11:09 +0900

    Test1 -> 소수찾기

--- src/dfs/Permutation.java
+++ src/dfs/Permutation.java
@@ -1,3 +1,5 @@
+package dfs;
+
 import java.util.Stack;
 
 public class Permutation {
@@ -6,25 +8,44 @@ public class Permutation {
 
     public static void main(String[] args) {
         n = 3;
-        chosen = new boolean[n+1];
+        chosen = new boolean[n];
         final Stack<Integer> stack = new Stack<>();
-        search(stack);
+        search_yy(stack);
     }
+
+    private static void search_yy(Stack<Integer> stack){
+        if(stack.size()==n){
+            System.out.println(stack.toString());
+            return;
+        }
+        for(int i=1;i<=n; i++){
+            if(chosen[i-1]) continue;
+
+            chosen[i-1] = true;
+            stack.push(i);
+
+            search_yy(stack);
+            chosen[i-1]=false;
+            stack.pop();
+
+        }
+    }
+
 
     private static void search(Stack<Integer> stack) {
         if (stack.size() >= n) {
             System.out.println(stack.toString());
         } else {
             for(int i=1; i<=n; i++) {
-                if(chosen[i]) continue; // 이미 뽑은 경우는 SKIP
+                if(chosen[i-1]) continue; // 이미 뽑은 경우는 SKIP
 
 
-                chosen[i] = true; // 포함
+                chosen[i-1] = true; // 포함
                 stack.push(i);
 
                 search(stack);
 
-                chosen[i] = false; // 제외
+                chosen[i-1] = false; // 제외
                 stack.pop();
             }
         }
---
+++ src/dfs/Test1.java
@@ -0,0 +1,70 @@
+package dfs;
+
+import java.util.*;
+class Test1 {
+    Stack<String> stack;
+    Set<Integer> set =new HashSet<>();
+    boolean[] visited;
+    int answer = 0;
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println(test1.solution("011"));
+    }
+    public int solution(String numbers) {
+
+        for(int i=1; i<=numbers.length(); i++){
+            stack = new Stack<>();
+            visited = new boolean[numbers.length()];
+            makeNum(i, numbers);
+        }
+
+        System.out.println("set = " + set);
+        for(int num : set){
+            if(sosuYn(num)){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    public boolean sosuYn(int x){
+        boolean result = true;
+        if(x==0 || x==1) return false;
+        if(x==2) return true;
+        for(int i=2; i<=Math.sqrt(x); i++){
+            if(x%i==0){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    //r:  자리수, number 숫자
+    public void makeNum(int r,  String number){
+        if(stack.size() == r){
+
+            //System.out.println(stack.toString());
+            List<String> copyStack = (List) stack.clone();
+            String num = "";
+            for(String data : copyStack){
+                num += data;
+            }
+            //System.out.println(Integer.parseInt(num));
+            /*if(sosuYn(Integer.parseInt(num))){
+                answer++;
+            }*/
+            set.add(Integer.parseInt(num));
+        }else{
+            for(int i=0; i<number.length(); i++){
+                if(visited[i]) continue;
+                stack.push(number.charAt(i)+"");
+                visited[i]=true;
+                makeNum(r, number);
+                visited[i]=false;
+                stack.pop();
+            }
+        }
+    }
+
+}
