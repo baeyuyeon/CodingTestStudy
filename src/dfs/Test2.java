a13e0b166588c4d7a5e85ea3877a470159f77632
baeyuyeon <dbdus983@naver.com>
2023-03-26 16:47:46 +0900

    Test2 -> 카펫 문제

---
+++ src/dfs/Test2.java
@@ -0,0 +1,21 @@
+package dfs;
+
+class Test2 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        final int moseri = 4; // 갈색이 채워질 수
+        for(int i=1; i<=yellow; i++){// i를 세로로 둠.
+            if(yellow%i ==0){
+                int x = yellow/i; // x:가로
+
+                if(i*2+x*2+moseri == brown){
+                    answer[0] = x+2;
+                    answer[1] = i+2;
+                    break;
+                }
+            }
+
+        }
+        return answer;
+    }
+}
