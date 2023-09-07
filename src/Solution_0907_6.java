5c46f5bdbe73b7a1eb2a16c66cbf81531023d830
baeyuyeon <31380511+baeyuyeon@users.noreply.github.com>
2023-09-07 23:08:04 +0900

    0907 코테연습2

---
+++ src/Solution_0907_6.java
@@ -0,0 +1,17 @@
+public class Solution_0907_6 {
+
+    public static void main(String[] args) {
+        int age = 23;
+        System.out.println(solution(age));
+    }
+
+    public static String solution(int age) {
+        String answer = "";
+        String strAge = Integer.toString(age);
+        for (int i = 0; i < strAge.length(); i++) {
+            char c = strAge.charAt(i);
+            answer += (char) ('a' + Integer.parseInt(String.valueOf(c)));
+        }
+        return answer;
+    }
+}
